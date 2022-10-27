b9bdd4c5fd93ae958900a69e0ffb949d804b0779
AldoRiveraRamirez <aldonicolasriverarnz@gmail.com>
2022-10-26 19:46:31 -0500

    Agregar archivos de proyecto.

---
+++ Ejemplo1Asp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejemplo1Asp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()//cuando estan dentro de un controlador son acciones 
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "IEVN 1001";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Peliculas()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Suma()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Ejemplo1Asp/Controllers/OperasBasController.cs
@@ -0,0 +1,66 @@
+﻿using Ejemplo1Asp.Models;
+using Ejemplo1Asp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejemplo1Asp.Controllers
+{
+    public class OperasBasController : Controller
+    {
+        //  GET: OperasBas
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MuestraPeliculas()
+        {
+            var PeliculasService = new PeliculasService();
+            var model = PeliculasService.ObtenerPelicula();
+            return View(model);
+        }
+        public ActionResult Sumar()
+        {
+            var tem = new Calculos();
+            return View(tem);
+        }
+        [HttpPost]
+            public ActionResult Sumar(Calculos calculos, string r1)
+        {
+
+            var tem = new Calculos();
+            tem = calculos;
+            if (r1.Equals("Sumar"))
+            {
+                tem.Suma();
+            }
+            if (r1.Equals("Restar"))
+            {
+                tem.Resta();
+            }
+
+
+            ViewBag.res = tem.Resultado;
+            return View();
+        }
+        public RedirectToRouteResult Temporal()
+        {
+
+            return RedirectToAction("About", "Home");
+            // var alumno1 = new Alumno() { Nombre = "Iza", Edad = 15 };
+            //var alumno2 = new Alumno() { Nombre = "Sofia", Edad = 16 };
+            //return Json(new List<Alumno>() { alumno1, alumno2}, JsonRequestBehavior.AllowGet);
+            //  return Content("Hola Mundo");
+
+        }
+
+        public ActionResult vista()
+        {
+            ViewBag.Nombre = "Sofia";
+            ViewData["Nom"] = "Iza";
+            return View();
+        }
+    }
+}
